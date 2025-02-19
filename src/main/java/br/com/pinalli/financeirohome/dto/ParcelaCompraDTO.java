2355ff339ec26ed9c3e49aa1c9b5010f3334ef65
pinalli <betopinali@gmail.com>
2025-02-18 22:07:00 -0300

    refactor: substituir DTO por record para imutabilidade e concisão

--- src/main/java/br/com/pinalli/financeirohome/dto/ParcelaCompraDTO.java
+++ src/main/java/br/com/pinalli/financeirohome/dto/ParcelaCompraDTO.java
@@ -1,18 +1,14 @@
 package br.com.pinalli.financeirohome.dto;
 
 import br.com.pinalli.financeirohome.model.ParcelaCompra;
-import lombok.Builder;
-import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Data
-@Builder
-public class ParcelaCompraDTO {
-    private Long id;
-    private BigDecimal valorParcela;
-    private LocalDate dataVencimento;
-    private LocalDate dataPagamento;
-    private ParcelaCompra status;
-}
+public record ParcelaCompraDTO (
+        Long id,
+        BigDecimal valorParcela,
+        LocalDate dataVencimento,
+        LocalDate dataPagamento,
+        ParcelaCompra status
+){}
